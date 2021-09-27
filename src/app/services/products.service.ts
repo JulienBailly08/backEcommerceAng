58678e9932519622b150fe18d275b413b00b6370
Julien_B <julienbailly08@gmail.com>
2021-09-27 16:44:40 +0200

    creation lien vers api via service et methode de contact

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  private baseUrl = `${environment.api+'products'+'?API_KEY='+environment.api_key}`;
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() : Observable<any>{
+    return this.http.get(this.baseUrl);
+  }
 }
