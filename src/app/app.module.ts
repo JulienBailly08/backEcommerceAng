68056981b855cc6ef0951bade34078f69c514d1b
Julien_B <julienbailly08@gmail.com>
2021-09-27 12:24:52 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DeleteProductModalComponent } from './delete-product-modal/delete-product-modal.component';
+import { AddOrEditProductModalComponent } from './add-or-edit-product-modal/add-or-edit-product-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DeleteProductModalComponent,
+    AddOrEditProductModalComponent
   ],
   imports: [
     BrowserModule,
