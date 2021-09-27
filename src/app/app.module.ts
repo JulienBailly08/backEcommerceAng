c6a647733d25f0dd816f4145b23889b864bd6d10
Julien_B <julienbailly08@gmail.com>
2021-09-27 12:44:58 +0200

    modif components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { DeleteProductModalComponent } from './delete-product-modal/delete-product-modal.component';
-import { AddOrEditProductModalComponent } from './add-or-edit-product-modal/add-or-edit-product-modal.component';
+import { HomeComponent } from './components/home/home.component';
+import { DeleteProductModalComponent } from './components/delete-product-modal/delete-product-modal.component';
+import { AddOrEditProductModalComponent } from './components/add-or-edit-product-modal/add-or-edit-product-modal.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     DeleteProductModalComponent,
-    AddOrEditProductModalComponent
+    AddOrEditProductModalComponent,
   ],
   imports: [
     BrowserModule,
