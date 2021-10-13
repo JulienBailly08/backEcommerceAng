5092f06c1cbdf36eac68f7e82729efa890b5195e
Julien_B <julienbailly08@gmail.com>
2021-10-13 15:14:15 +0200

    ajout categories depuis service vers le formulaire

--- src/app/components/add-or-edit-product-modal/add-or-edit-product-modal.component.ts
+++ src/app/components/add-or-edit-product-modal/add-or-edit-product-modal.component.ts
@@ -1,18 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Category } from 'src/app/models/category';
 import { Product } from 'src/app/models/product';
+import { CategoriesService } from 'src/app/services/categories.service';
+
 
 @Component({
   selector: 'app-add-or-edit-product-modal',
   templateUrl: './add-or-edit-product-modal.component.html',
   styleUrls: ['./add-or-edit-product-modal.component.css']
 })
-export class AddOrEditProductModalComponent implements OnInit {
+export class AddOrEditProductModalComponent implements OnInit, OnDestroy {
 
   @Input() product!:Product;
   productForm !: FormGroup;
+  categories!: Category[];
+  categorySub!: Subscription;
+  idCategory = 1;
 
-  constructor(private formBuilder:FormBuilder) {
+  constructor(
+    private formBuilder:FormBuilder,
+    private categoriesServices:CategoriesService
+  ) {
+
     this.productForm = formBuilder.group({
       productInfos: formBuilder.group({
         name:['',Validators.required],
@@ -26,7 +37,19 @@ export class AddOrEditProductModalComponent implements OnInit {
     })
   }
 
+  selectCategory(id:number){
+    this.idCategory=id;
+  }
+
   ngOnInit(): void {
+    this.categorySub = this.categoriesServices.getCategories().subscribe(
+      (response)=>{
+        this.categories = response.result;
+      }
+    );
+  }
+  ngOnDestroy(): void {
+    this.categorySub.unsubscribe;
   }
 
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Response } from 'src/app/models/response';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  private baseUrl = `${environment.api + 'category' + '?API_KEY=' + environment.api_key}`;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getCategories() : Observable < Response > {
+    return this.http.get<Response>(this.baseUrl);
+  }
 }
